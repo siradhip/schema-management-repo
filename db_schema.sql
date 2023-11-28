4eb20b843e4fcac5649879da6f96dde5b85ebad9
Aparna Chandramohan <Aparna@Aparnas-MacBook-Air.local>
2023-11-28 12:16:54 -0500

    v3: fixed syntax issues

--- db_schema.sql
+++ db_schema.sql
@@ -2,7 +2,7 @@
 
 -- drop redundant column
 alter table albums
-drop column "album_id album artist_name release_date genres average_rating n";
+drop column "album_id	album	artist_name release_date	genres	average_rating	n";
 
 -- add unique index concurrently
 create unique index concurrently pk_album
@@ -10,7 +10,7 @@ on albums(album_id);
 
 -- create primary key using index
 alter table albums
-add primary key using pk_album;
+add primary key using index pk_album;
 
 -- add album index to get album details
 create index concurrently idx_album
